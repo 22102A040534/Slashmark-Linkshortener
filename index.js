3f8f2266442499ac52c5432e66b1093fa7116c4e
ARSHIA NOORAIEN <142517895+22102A040534@users.noreply.github.com>
2024-07-26 23:51:15 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.port;
+const host = process.env.host;
+const bodyParser = require("body-parser"); //use to parse incoming request bodies
+const services = require("./routes/service");
+const db = require("./data/db");
+const urlDb = require("./data/url");
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.listen(port, () => console.log("listening port " + port));
